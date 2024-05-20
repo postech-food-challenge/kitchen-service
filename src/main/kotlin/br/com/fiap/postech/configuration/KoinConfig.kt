6760ed4f4be2e5850700767cbe1801b911fc1cfc
dvdonadelli <davi_donadelli@live.com>
2024-05-20 17:31:58 -0300

    refactor: koin dependency injection

--- src/main/kotlin/br/com/fiap/postech/configuration/KoinConfig.kt
+++ src/main/kotlin/br/com/fiap/postech/configuration/KoinConfig.kt
@@ -1,15 +1,16 @@
 package br.com.fiap.postech.configuration
 
+import br.com.fiap.postech.application.gateways.OrderGateway
 import br.com.fiap.postech.application.usecases.ListOrdersInteract
 import br.com.fiap.postech.application.usecases.SendPatchRequestInteract
 import br.com.fiap.postech.application.usecases.StartOrderInteract
 import br.com.fiap.postech.application.usecases.UpdateOrderStatusInteract
 import br.com.fiap.postech.infraestructure.aws.DynamoDbClientProvider
 import br.com.fiap.postech.infraestructure.gateways.OrderGatewayImpl
+import br.com.fiap.postech.infraestructure.persistence.OrderRepository
 import br.com.fiap.postech.infraestructure.persistence.OrderRepositoryDynamoDbImpl
 import io.ktor.server.application.*
 import io.ktor.server.config.*
-import org.koin.core.module.Module
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
@@ -19,19 +20,23 @@ fun Application.configureKoin(config: ApplicationConfig) {
     }
 }
 
-fun getModules(config: ApplicationConfig): Module {
-    return module {
-        val dynamoClientProvider = DynamoDbClientProvider(
+fun getModules(config: ApplicationConfig) = module {
+    single {
+        DynamoDbClientProvider(
             config.property("aws.access_key_id").getString(),
             config.property("aws.secret_access_key").getString(),
             config.property("aws.aws_session_token").getString(),
             config.property("aws.region").getString()
         )
-        val orderRepository = OrderRepositoryDynamoDbImpl(dynamoClientProvider)
-        val orderGateway = OrderGatewayImpl(orderRepository)
-        single<OrderGatewayImpl> { orderGateway }
-        single<UpdateOrderStatusInteract> { UpdateOrderStatusInteract(orderGateway, SendPatchRequestInteract(config.property("orders-ms.complete_order_url").getString())) }
-        single<ListOrdersInteract> { ListOrdersInteract(orderGateway) }
-        single<StartOrderInteract> { StartOrderInteract(orderGateway) }
     }
+    single<OrderRepository> { OrderRepositoryDynamoDbImpl(get()) }
+    single<OrderGateway> { OrderGatewayImpl(get()) }
+    single {
+        UpdateOrderStatusInteract(
+            get(),
+            SendPatchRequestInteract(config.property("orders-ms.complete_order_url").getString())
+        )
+    }
+    single { ListOrdersInteract(get()) }
+    single { StartOrderInteract(get()) }
 }
