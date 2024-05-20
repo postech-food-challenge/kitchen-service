8eacc775542d7b41befb49854ca6571fc826fbd4
Thomaz Palmeira <palmeiirat@gmail.com>
2024-05-20 18:52:40 -0400

    fix: test

--- src/test/kotlin/steps/Config.kt
+++ src/test/kotlin/steps/Config.kt
@@ -6,16 +6,16 @@ import io.restassured.builder.RequestSpecBuilder
 import io.restassured.http.ContentType
 
 
-//class Config {
-//    @Before
-//    fun setup() {
-//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
-//
-//        RestAssured.baseURI = "http://localhost:8080"
-//        RestAssured.basePath = "/v1/kitchen"
-//
-//        RestAssured.requestSpecification = RequestSpecBuilder()
-//            .setContentType(ContentType.JSON)
-//            .build()
-//    }
-//}
+class Config {
+    @Before
+    fun setup() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+
+        RestAssured.baseURI = "http://localhost:8080"
+        RestAssured.basePath = "/v1/kitchen"
+
+        RestAssured.requestSpecification = RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .build()
+    }
+}
