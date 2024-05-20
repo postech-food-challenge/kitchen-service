6c6c9350e28ccd51853c4a9f451fb13dae596477
Thomaz Palmeira <palmeiirat@gmail.com>
2024-05-20 17:57:12 -0400

    fix: fixing host from server used for tests and adjusting Dockerfile script to run tests

--- src/test/kotlin/steps/ListOrdersSteps.kt
+++ src/test/kotlin/steps/ListOrdersSteps.kt
@@ -8,61 +8,60 @@ import io.restassured.response.Response
 import org.json.JSONArray
 import org.json.JSONObject
 import kotlin.test.assertTrue
-//
-//class ListOrdersSteps {
-//
-//    private var currentStatusOnRequest: String? = null
-//    private lateinit var response: Response
-//
-//    @Given("I have a few orders on my database")
-//    fun i_have_a_few_orders_on_my_database() {
-//    }
-//
-//    @When("I try to find orders on status = {word}")
-//    fun i_try_to_find_orders_on_status(status: String) {
-//        currentStatusOnRequest = status
-//        response = RestAssured.
-//            given()
-//                .queryParam("status", status)
-//                .`when`()
-//                .get()
-//    }
-//
-//    @Then("The system should warn me that the status is invalid2")
-//    fun the_system_should_throw_me_an_error2() {
-//        val jsObject = JSONObject(response.body().asPrettyString())
-//        val map = jsObject.toMap()
-//
-//        assertTrue { map.containsValue("Invalid status: $currentStatusOnRequest") }
-//    }
-//
-//    @When("I try to find orders without status filter")
-//    fun i_try_to_find_orders_without_status_filter() {
-//        currentStatusOnRequest = null
-//        response =
-//            RestAssured.get()
-//    }
-//
-//    @Then("The system should return all orders whose status aren't on the list")
-//    fun the_system_should_return_all_orders_whose_status_aren_t_on_the_list(statusInativos: List<String>) {
-//        val jsArray = JSONArray(response.body().asPrettyString())
-//
-//        val count = (0 until jsArray.length())
-//            .map { jsArray.getJSONObject(it) }
-//            .count { it.optString("status") in statusInativos }
-//
-//        assertTrue { count == 0 }
-//    }
-//
-//    @Then("The system should return all orders on {word} status")
-//    fun the_system_should_return_all_orders_on_status(status: String) {
-//        val jsArray = JSONArray(response.body().asPrettyString())
-//
-//        val count = (0 until jsArray.length())
-//            .map { jsArray.getJSONObject(it) }
-//            .count { it.optString("status") == status }
-//
-//        assertTrue { jsArray.length() == count }
-//    }
-//
-//}
+
+class ListOrdersSteps {
+
+    private var currentStatusOnRequest: String? = null
+    private lateinit var response: Response
+
+    @Given("I have a few orders on my database")
+    fun i_have_a_few_orders_on_my_database() {
+    }
+
+    @When("I try to find orders on status = {word}")
+    fun i_try_to_find_orders_on_status(status: String) {
+        currentStatusOnRequest = status
+        response = RestAssured.given()
+            .queryParam("status", status)
+            .`when`()
+            .get()
+    }
+
+    @Then("The system should warn me that the status is invalid2")
+    fun the_system_should_throw_me_an_error2() {
+        val jsObject = JSONObject(response.body().asPrettyString())
+        val map = jsObject.toMap()
+
+        assertTrue { map.containsValue("Invalid status: $currentStatusOnRequest") }
+    }
+
+    @When("I try to find orders without status filter")
+    fun i_try_to_find_orders_without_status_filter() {
+        currentStatusOnRequest = null
+        response =
+            RestAssured.get()
+    }
+
+    @Then("The system should return all orders whose status aren't on the list")
+    fun the_system_should_return_all_orders_whose_status_aren_t_on_the_list(statusInativos: List<String>) {
+        val jsArray = JSONArray(response.body().asPrettyString())
+
+        val count = (0 until jsArray.length())
+            .map { jsArray.getJSONObject(it) }
+            .count { it.optString("status") in statusInativos }
+
+        assertTrue { count == 0 }
+    }
+
+    @Then("The system should return all orders on {word} status")
+    fun the_system_should_return_all_orders_on_status(status: String) {
+        val jsArray = JSONArray(response.body().asPrettyString())
+
+        val count = (0 until jsArray.length())
+            .map { jsArray.getJSONObject(it) }
+            .count { it.optString("status") == status }
+
+        assertTrue { jsArray.length() == count }
+    }
+
+}
--- src/test/kotlin/steps/UpdateOrderStatusSteps.kt
+++ src/test/kotlin/steps/UpdateOrderStatusSteps.kt
@@ -1,8 +1,6 @@
 package steps
 
-import br.com.fiap.postech.domain.entities.Order
 import br.com.fiap.postech.domain.entities.OrderItem
-import br.com.fiap.postech.domain.entities.OrderStatus
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
@@ -12,73 +10,73 @@ import org.json.JSONObject
 import java.util.*
 import kotlin.test.assertTrue
 
-//class UpdateOrderStatusSteps {
-//
-//    private var currentOrderId = ""
-//    private var createdOrderId: UUID? = null
-//    private lateinit var currentStatusOnRequest: String
-//    private lateinit var response: Response
-//
-//    @Given("I'm trying to update the status of an order with id = {word}")
-//    fun im_trying_to_update_the_status_of_an_order_with_id(id: String) {
-//        currentOrderId = id
-//    }
-//
-//    @Given("I created an order and I'm trying to update it's status")
-//    fun i_created_an_order_and_im_trying_to_update_the_status_of_an_order_with_id() {
-//        createdOrderId = UUID.randomUUID();
-//        currentOrderId = createdOrderId.toString()
-//
-//        val item = OrderItem(
-//            "Test item",
-//            quantity = 1,
-//            toGo = true,
-//            observations = "sem cebola"
-//        )
-//
-//        given().body(listOf(item)).`when`().post("/start/$createdOrderId")
-//
-//    }
-//
-//    @When("I try to change it's status to {word}")
-//    fun i_try_to_change_its_status(status: String) {
-//        val url = "/$currentOrderId"
-//        val body = "{ \"status\": \"${status}\" }"
-//
-//        currentStatusOnRequest = status
-//        response =
-//            given()
-//                .body(body)
-//                .`when`()
-//                .patch(url)
-//    }
-//
-//    @Then("The system should warn me that there's no such order")
-//    fun b() {
-//        val jsObject = JSONObject(response.body().asPrettyString())
-//        val map = jsObject.toMap()
-//
-//        assertTrue { map.containsValue("No order found for id = $currentOrderId") }
-//    }
-//
-//    @Then("The system should warn me that the status is invalid")
-//    fun the_system_should_throw_me_an_error() {
-//        val jsObject = JSONObject(response.body().asPrettyString())
-//        val map = jsObject.toMap()
-//
-//        assertTrue { map.containsValue("Invalid status: $currentStatusOnRequest") }
-//    }
-//
-//    @Then("I should receive the order with it's status set to {word}")
-//    fun i_should_receive_the_order_with_it_s_status_set_to_canceled(expectedStatus: String) {
-//        val jsObject = JSONObject(response.body().asPrettyString())
-//        val map = jsObject.toMap()
-//
-//        assertTrue { map["status"]?.equals(expectedStatus) ?: false }
-//    }
-//
-//    @Then("I should receive an error stating that the id I passed is invalid")
-//    fun i_should_receive_an_error_stating_that_the_id_i_passes_is_invalid() {
-//        assertTrue { response.body().asPrettyString().equals("Invalid ID") ?: false }
-//    }
-//}
+class UpdateOrderStatusSteps {
+
+    private var currentOrderId = ""
+    private var createdOrderId: UUID? = null
+    private lateinit var currentStatusOnRequest: String
+    private lateinit var response: Response
+
+    @Given("I'm trying to update the status of an order with id = {word}")
+    fun im_trying_to_update_the_status_of_an_order_with_id(id: String) {
+        currentOrderId = id
+    }
+
+    @Given("I created an order and I'm trying to update it's status")
+    fun i_created_an_order_and_im_trying_to_update_the_status_of_an_order_with_id() {
+        createdOrderId = UUID.randomUUID();
+        currentOrderId = createdOrderId.toString()
+
+        val item = OrderItem(
+            "Test item",
+            quantity = 1,
+            toGo = true,
+            observations = "sem cebola"
+        )
+
+        given().body(listOf(item)).`when`().post("/start/$createdOrderId")
+
+    }
+
+    @When("I try to change it's status to {word}")
+    fun i_try_to_change_its_status(status: String) {
+        val url = "/$currentOrderId"
+        val body = "{ \"status\": \"${status}\" }"
+
+        currentStatusOnRequest = status
+        response =
+            given()
+                .body(body)
+                .`when`()
+                .patch(url)
+    }
+
+    @Then("The system should warn me that there's no such order")
+    fun b() {
+        val jsObject = JSONObject(response.body().asPrettyString())
+        val map = jsObject.toMap()
+
+        assertTrue { map.containsValue("No order found for id = $currentOrderId") }
+    }
+
+    @Then("The system should warn me that the status is invalid")
+    fun the_system_should_throw_me_an_error() {
+        val jsObject = JSONObject(response.body().asPrettyString())
+        val map = jsObject.toMap()
+
+        assertTrue { map.containsValue("Invalid status: $currentStatusOnRequest") }
+    }
+
+    @Then("I should receive the order with it's status set to {word}")
+    fun i_should_receive_the_order_with_it_s_status_set_to_canceled(expectedStatus: String) {
+        val jsObject = JSONObject(response.body().asPrettyString())
+        val map = jsObject.toMap()
+
+        assertTrue { map["status"]?.equals(expectedStatus) ?: false }
+    }
+
+    @Then("I should receive an error stating that the id I passed is invalid")
+    fun i_should_receive_an_error_stating_that_the_id_i_passes_is_invalid() {
+        assertTrue { response.body().asPrettyString().equals("Invalid ID") ?: false }
+    }
+}
